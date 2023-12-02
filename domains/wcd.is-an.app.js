da4d43cf467ebd04b13be17bc6b3b02260676046
WangChuDi <1224918101@qq.com>
2023-12-02 17:59:09 +0800

    Create wcd.is-an.app

---
+++ domains/wcd.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'To convert Clashsub',
+  domain: 'is-an.app',
+  subdomain: 'wcd',
+  owner: {
+    repo: 'https://github.com/WangChuDi/SubConv/tree/main',
+    email: '1224918101@qq.com',
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com',
+  },
+  proxy: false,
+})
